9ab72334ba4ec71d5b7e0d8f9961175901a9b07d
dotNulled <20d.dhruv@gmail.com>
2024-11-06 20:27:16 +0530

    Add functions for database I/O
    Add functionality to interface with the database and update the website UI accordingly

--- index.js
+++ index.js
@@ -3,24 +3,77 @@ const app = express();
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const path = require('path');
-const morgan = require('morgan');
-const nunjucks = require('nunjucks');
 
-const env = new nunjucks.Environment();
+const database_path = './database/tasks.json';
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
 app.use(express.static('static'));
-var accessLogStream = fs.createWriteStream(path.join(__dirname, 'logs/logs.log'), { flags: 'a' })
-app.use(morgan("[:date[web]]    :    :method  |  ':url'  |   HTTP/:http-version   |  Code :status  |  :response-time ms", { stream: accessLogStream }))
-app.use(express.json())
-nunjucks.configure('templates', {
-    autoescape: true,
-    express: app
+app.use(express.json());
+
+// Database I/O functions:
+function read_database(path_to_database = database_path, format = 'utf8') {
+    return JSON.parse(fs.readFileSync(path_to_database, format))
+}
+
+function write_to_database(path_to_database = database_path, new_data, format = 'utf8') {
+    fs.writeFileSync(path_to_database, JSON.stringify(new_data, null, 2), format)
+}
+
+function update_database(task, complete, path_to_database = database_path) {
+    let new_data = {
+        'current': task,
+        'completed': complete
+    }
+    write_to_database(path_to_database, new_data)
+}
+
+// Update the local-state variables to that of the JSON database
+let initial_data = read_database(database_path);
+var task = initial_data.current;
+var complete = initial_data.completed;
+
+//post route for adding new task 
+app.post("/addtask", function (req, res) {
+    var newTask = req.body.newtask;
+    task.push(newTask);
+    update_database(task, complete);
+    res.redirect("/");
 });
 
+//post route for removing a task
+app.post("/removetask", function (req, res) {
+    var completeTask = req.body.check;
+    if (typeof completeTask === "string") {
+        complete.push(completeTask);
+        task.splice(task.indexOf(completeTask), 1);
+    } else if (typeof completeTask === "object") {
+        for (var i = 0; i < completeTask.length; i++) {
+            complete.push(completeTask[i]);
+            task.splice(task.indexOf(completeTask[i]), 1);
+        }
+    }
+    update_database(task, complete);
+    res.redirect("/");
+});
+
+//get route to default homepage
 app.get('/', (req, res) => {
-    data = {}
-    res.render('home.html', data);
+    data = {
+        task: task,
+        complete: complete,
+        current_task: "sample task" //TODO: Change to a function update from ML model
+    }
+    res.render('home', data);
+});
+
+//get route to tasks/events history page
+app.get('/history', (req, res) => {
+    data = {
+        task: task,
+        complete: complete
+    }
+    res.render('history', data);
 });
 
 const port = 3000
